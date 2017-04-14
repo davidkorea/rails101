64517f074996ca5064b320ab038fd52094e455c6
OS X <lyfirst@naver.com>
2017-04-15 00:04:37 +0900

    check owner permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 
 
   def index
@@ -12,6 +12,10 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
   end
 
 
@@ -34,6 +38,11 @@ class GroupsController < ApplicationController
    def update
      @group = Group.find(params[:id])
 
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
+    end
+
+
      if @group.update(group_params)
        redirect_to groups_path, notice: "Update Success"
      else
@@ -45,6 +54,11 @@ class GroupsController < ApplicationController
 
    def destroy
       @group = Group.find(params[:id])
+
+      if current_user != @group.user
+        redirect_to root_path, alert: "You have no permission."
+     end
+
       @group.destroy
       flash[:alert] = "Device Deleted"
       redirect_to groups_path
